17b8d497e39e25506c9c766ab61d64da792bfce9
Hurmatullah <hrasaa1@gmail.com>
2022-06-23 17:26:24 +0300

    Update react code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import createRoot from "react-dom";
 
-function Greeting(){
-
-  return <h1>Hey this is my first code after a while, please check it out</h1>
-
+function Greeting() {
+  return <h1>Hello world</h1>;
 }
 
-createRoot.render(<Greeting/>, document.getElementById('root'));
+createRoot.render(<Greeting />, document.getElementById("root"));
