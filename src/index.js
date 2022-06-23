beae095e4f67a519aa16d3b19b9547a08586de02
Hurmatullah <hrasaa1@gmail.com>
2022-06-23 17:30:40 +0300

    Updated react todo code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import createRoot from "react-dom";
 function Greeting() {
   return (
     <div>
-      <h3>Hey babe</h3>
-      <h4>Hurrrrraaaaa</h4>
+      <Babe></Babe>
+      <Hurmat></Hurmat>
     </div>
   );
 }
+
+const Babe = () => <p>Hello my Roya</p>;
+const Hurmat = () => (
+  <ul>
+    <li>Hey my name is hurmat</li>
+  </ul>
+);
 
 createRoot.render(<Greeting />, document.getElementById("root"));
