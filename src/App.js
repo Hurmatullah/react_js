0dd412a7829deb7bcbcb29b4330c20207040a3e8
Hurmatullah <hrasaa1@gmail.com>
2022-06-27 16:03:23 +0300

    Updated some codes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h2>Welcome in this tutorial</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from "react";
 import createRoot from "react-dom";
+import App from "./App";
 
-const bookName = "How to love a girl";
-const author = "Hurmat";
-const photo =
-  "https://image.shutterstock.com/image-illustration/stack-textbooks-isolated-on-white-260nw-83105218.jpg";
-
-function Booklists() {
-  return <Books></Books>;
-}
-function Books() {
-  return (
-    <article>
-      <img src={photo} alt=""></img>
-      <h4>{bookName}</h4>
-      <h4>{author}</h4>
-    </article>
-  );
-}
-
-createRoot.render(<Booklists />, document.getElementById("root"));
+createRoot.render(<App />, document.getElementById("root"));
