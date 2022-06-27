3d98ae45556be704c81b741372bc87d975d4f939
Hurmatullah <hrasaa1@gmail.com>
2022-06-27 18:34:55 +0300

    Clicked Event

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,19 @@
+const handleClick = () => {
+  console.log("Congratulations, you have clicked me");
+};
+
+const handleClickAgain = (name) => {
+  console.log("you clicked me again" + " " + name);
+};
+
 const Home = () => {
-  return <h2>Home page</h2>;
+  return (
+    <div>
+      <h2>Hello world</h2>
+      <button onClick={handleClick}>CLick me</button>
+      <button onClick={() => handleClickAgain("Mario")}>Click again</button>
+    </div>
+  );
 };
 
 export default Home;
