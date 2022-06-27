bedef2bdde5852afceaaa31b9564f101b7648832
Hurmatullah <hrasaa1@gmail.com>
2022-06-27 18:10:22 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
         <h2>Welcome in this tutorial</h2>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import createRoot from "react-dom";
 import App from "./App";
+import Navbar from "./Navbar";
 
 createRoot.render(<App />, document.getElementById("root"));
