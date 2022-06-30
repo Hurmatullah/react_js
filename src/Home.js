b4e53c509753fa6e9b9048ff532794f899172aad
Hurmatullah <hrasaa1@gmail.com>
2022-06-30 20:08:19 +0300

    Refactored code

--- src/Home.js
+++ src/Home.js
@@ -34,17 +34,42 @@ import Bloglist from "./Bloglist";
 
 const Home = () => {
   const [blogs, setBLogs] = useState(null);
-  
+  const [pending, setPendings] = useState(true);
+  const [error, setErrors] = useState(null);
 
   const deleteRow = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBLogs(newBlogs);
   };
 
+  useEffect(() => {
+
+    setTimeout(() => {
+      fetch("http://localhost:9000/posts")
+      .then(res => {
+        if(!res.ok){
+         throw Error("I am sorry for now we are not able to fetch your data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBLogs(data);
+        setPendings(false);
+        setErrors(null);
+      }).catch(err => {
+        setErrors("I am sorry, for now we are not able to fetch your data");
+        setPendings(false);
+      })
+    }, 1000);
+
+
+  })
 
   return (
     <div className="Home">
-      <Bloglist blogs={blogs}title="All title"></Bloglist>
+      {pending && <div>Loading values</div>}
+      {error && <div>{error}</div>}
+      {blogs && <Bloglist blogs={blogs}title="All title"></Bloglist>}
     </div>
   );
 };
