e3f4af15edad73e9099e494e000ab0d272e2f242
Hurmatullah <hrasaa1@gmail.com>
2022-07-05 15:20:11 +0300

    React course finished

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The dojo tutorial</h1>
+      <h1>React tutorial</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">Create Blog</Link>
