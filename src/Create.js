344df8b2648fc8d77c02d695ad40b201f2aef2b8
Hurmatullah <hrasaa1@gmail.com>
2022-07-05 12:50:09 +0300

    Updated code

--- src/Create.js
+++ src/Create.js
@@ -1,15 +1,21 @@
 const Create = () => {
-
-    return (
-
-        <h2>This is create page!</h2>
-
-
-    );
-
-
-}
-
-
+  return (
+    <div className="Create-blog">
+      <h2>Add new blog</h2>
+      <form>
+        <label>Title</label>
+        <input type="text" required />
+        <label>Content</label>
+        <textarea required></textarea>
+        <label>Author</label>
+        <select>
+          <option>Josi</option>
+          <option>Yosi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+    </div>
+  );
+};
 
 export default Create;
