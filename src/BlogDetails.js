c936651bda31eb1df6dca14bfdd43619959b034a
Hurmatullah <hrasaa1@gmail.com>
2022-07-05 14:18:13 +0300

    Delete function executed in code

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,28 +1,39 @@
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import useFetch from "./useFetch";
+import {useHistory} from "react-router-dom";
 
 const BlogDetails = () => {
+  const {id} = useParams();
+  const {
+    data: blog,
+    pending,
+    error,
+  } = useFetch("http://localhost:9000/posts/" + id);
+  const history = useHistory();
 
-    const { id } = useParams();
-    const {data:blog, pending, error} = useFetch("http://localhost:9000/posts/" + id);
+  const handleDelete = () => {
+    setTimeout(() => {
+      fetch("http://localhost:9000/posts/" + blog.id, {
+        method: "DELETE",
+      }).then(() => {
+        history.push("/");
+      });
+    }, 1000);
+  };
 
-    return (
-        
-      <div className="blog-details">
-        {pending && <div>Please wait</div>}
-        {error && <div>You have some problem!</div>}
-        {blog && 
+  return (
+    <div className="blog-details">
+      {pending && <div>Please wait</div>}
+      {error && <div>You have some problem!</div>}
+      {blog && (
         <article>
-           <h2> { blog.title } </h2>
-           <p> { blog.author } </p>
+          <h2> {blog.title} </h2>
+          <p> {blog.author} </p>
+          <button onClick={handleDelete}>Delete</button>
         </article>
-            
-        }
-      </div>
-
-    );
-
-
-}
+      )}
+    </div>
+  );
+};
 
 export default BlogDetails;
